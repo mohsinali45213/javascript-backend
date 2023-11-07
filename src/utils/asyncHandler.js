d141ba9793c745767998eda5c2068a7526c45481
mohsinali45213 <mohsinali45213@gmail.com>
2023-11-07 09:20:53 +0530

    H

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,14 +1,12 @@
-const asyncHandler =(requestHandler)=>{
-  (req,res,next)=>{
-    Promise.resolve(requestHandler(req,res,next))
-    .catch((error)=>next(error))
-  }
-}
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
 
-
-
-
-asyncHandler()
+asyncHandler();
 
 // const asyncHandler = (fn) => async(req,res,next)=>{
 //   try {
