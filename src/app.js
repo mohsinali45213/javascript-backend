3a112b867d46bc652638c1cb6bde7a52a2385053
mohsinali45213 <mohsinali45213@gmail.com>
2023-11-21 22:28:55 +0530

    route

--- src/app.js
+++ src/app.js
@@ -15,4 +15,13 @@ app.use(express.json({limit:"20kb"}))
 app.use(express.urlencoded({extended:true,limit:"20kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-export { app };
+
+//routes
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use("api/v1/users",userRouter)
+
+//http://localhost:8000/api/v1/users/register
+
+export default app
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,12 @@
 const asyncHandler = (requestHandler) => {
-  return(req, res, next) => {                 //TODO: return
+  return (req, res, next) => {                
     Promise.resolve(requestHandler(req, res, next)).catch((error) =>
       next(error)
     )
   }
 }
 
-asyncHandler();
+export {asyncHandler}
 
 // const asyncHandler = (fn) => async(req,res,next)=>{
 //   try {
